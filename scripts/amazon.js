fef5ebdb9cdc358fd653c3a61cad71ebd82f7529
Ansh Jagdish Pokar <anshjpokar@gmail.com>
2025-01-03 03:08:33 +0530

    .

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach((product)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
             Add to Cart
           </button>
         </div>`;
@@ -55,3 +55,30 @@ products.forEach((product)=>{
 });
 
 document.querySelector('.js-product-grid').innerHTML = productHtml;
+
+
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button)=>{
+        button.addEventListener('click',()=>{
+            const productName=button.dataset.productName;
+            let matchingItem;
+            cart.forEach((item)=>{
+                if (productName === item.productName){
+                    matchingItem = item;
+                }
+            });
+
+            if (matchingItem){
+                matchingItem.quantity+=1;
+            } else {
+                cart.push({
+                    productName : productName,
+                    quantity : 1
+                });
+            }
+
+           
+            console.log(cart);
+        });
+    });
