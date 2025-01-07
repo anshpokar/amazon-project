6b6268b1c96711789b045b66bf5ec3eb5bf5dfa0
Ansh Jagdish Pokar <anshjpokar@gmail.com>
2025-01-08 01:50:48 +0530

    made the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach((product)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`;
@@ -61,10 +61,10 @@ document.querySelector('.js-product-grid').innerHTML = productHtml;
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button)=>{
         button.addEventListener('click',()=>{
-            const productName=button.dataset.productName;
+            const productId=button.dataset.productId;
             let matchingItem;
             cart.forEach((item)=>{
-                if (productName === item.productName){
+                if (productId === item.productId){
                     matchingItem = item;
                 }
             });
@@ -73,12 +73,16 @@ document.querySelectorAll('.js-add-to-cart')
                 matchingItem.quantity+=1;
             } else {
                 cart.push({
-                    productName : productName,
+                    productId : productId,
                     quantity : 1
                 });
             }
+            
+            let cartQuantity=0;
+             cart.forEach((item) => {
+              cartQuantity += item.quantity;
+             });
 
-
-            console.log(cart);
+            document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
         });
     });
