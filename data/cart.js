54b5f4f0ce80af952610de4d9fd6f9f7f2061984
Ansh Jagdish Pokar <anshjpokar@gmail.com>
2025-04-10 23:57:23 +0530

    save data and generate html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -3,13 +3,16 @@
  if (!cart){
     cart=[{
         productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity:2
+        quantity:2,
+        deliveryOptionId: '1'
      },{
         productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity:65
+        quantity:65,
+        deliveryOptionId: '2'
      },{
         productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity:65
+        quantity:65,
+        deliveryOptionId: '3'
      }];
  }
  
@@ -31,7 +34,8 @@
               } else {
                   cart.push({
                       productId : productId,
-                      quantity : 1
+                      quantity : 1,
+                      deliveryOptionId: '1'
                   });
               }
               saveToStorage()
