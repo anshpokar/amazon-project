4c18eb2474ff7983d3179af3ba397b8a0832b19d
Ansh Jagdish Pokar <anshjpokar@gmail.com>
2025-04-28 17:00:11 +0530

    learn proises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,52 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/car.js';
 //import '../data/backend-practice.js';
-import { loadProducts } from "../data/products.js";
 
-
-loadProducts(()=>{
+Promise.all([
+    new Promise((resolve)=>{
+        loadProducts( () =>{
+            resolve('value 1');
+        });
+    }),
+    new Promise ((resolve)=>{
+        loadCart(()=>{
+            resolve('value 2');
+        });
+    })
+]).then((value)=>{
+    console.log(value); 
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
+/*
+new Promise((resolve)=>{
+    loadProducts( () =>{
+        resolve();
+    });
+
+}).then(()=>{
+    return new Promise ((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    });
+
+}).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+    
+/*
+loadProducts(()=>{
+    loadCart(()=>{
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+*/
